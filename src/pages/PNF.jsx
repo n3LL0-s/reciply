89a25a40da743c4a0135421a0516d67011053e4e
n3LL0-s <cschilder113@gmail.com>
2022-10-31 10:16:14 +0200

    Revert "fixed 404 bug"
    This reverts commit 5eb84df8dadaca55b0d77a547b52ac87ac32a05a.

---
+++ src/pages/PNF.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function PNF() {
+  return (
+    <>
+      <div className="error">
+        <h3>Oops...</h3>
+        <p>Error 404: Page not found</p>
+      </div>
+    </>
+  );
+}
