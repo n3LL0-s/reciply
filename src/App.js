f971f45c6b33ab21ee191bd13f8a10e7134a2f67
n3LL0-s <cschilder113@gmail.com>
2022-10-31 10:24:52 +0200

    dfsfe

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import PNF from "./pages/PNF";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/*" element={<PNF />} />
       </Routes>
       <Footer />
     </Router>
